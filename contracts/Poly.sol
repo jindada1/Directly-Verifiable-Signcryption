7cead6f8cbe30a18974699d6df29414bea4f6e35
jindada1 <zou_406143883@qq.com>
2021-11-16 00:43:13 +0800

    插值

--- contracts/Poly.sol
+++ contracts/Poly.sol
@@ -1,8 +1,7 @@
 pragma solidity ^0.5.17;
 
-
 contract Poly {
-    
+
     /**
      * @notice Implementation of lagrange interpolation formula
      * @param X   - .
@@ -10,13 +9,117 @@ contract Poly {
      * @param x   - .
      * @return y  - calc y corresponding to given x.
      */
-    function Interpolate(
-        uint256[] memory1 X,
-        uint256[] memory1 Y,
-        uint256 x
-    ) public view returns (uint256[2] memory y)
-    {
-        
+    function InterpolateInt(
+        int256[] memory X,
+        int256[] memory Y,
+        int256 x
+    ) public pure returns (int256 y) {
+        require(X.length == Y.length);
+        y = 0;
+        for (uint256 i = 0; i < Y.length; i++) {
+            int256 b = Y[i];
+            for (uint256 j = 0; j < X.length; j++) {
+                if (i == j) continue;
+                b = (b * (x - X[j])) / (X[i] - X[j]);
+            }
+            y += b;
+        }
     }
 
+    /**
+     * @notice Implementation of lagrange interpolation formula
+     * @param X   - [整数部分, 小数部分, 整数部分，小数部分, ..., 整数部分, 小数部分].
+     * @param Y   - [整数部分, 小数部分, 整数部分，小数部分, ..., 整数部分, 小数部分],
+     *              point is ( double(X[0],X[1]), double(Y[0],Y[1]) )
+     * @param x   - [整数部分, 小数部分].
+     * @return y  - calc y corresponding to given x.
+     */
+    function Interpolate(
+        int256[] memory X,
+        int256[] memory Y,
+        int256[2] memory x
+    ) public view returns (int256[2] memory y) {
+        require(X.length == Y.length);
+        require(X.length % 2 == 0);
+
+        y = [int256(0), int256(0)];
+
+        for (uint256 i = 0; i < Y.length; i += 2) {
+            // b = Y[i];
+            int256[2] memory b = [Y[i], Y[i + 1]];
+
+            for (uint256 j = 0; j < X.length; j += 2) {
+                if (i == j) continue;
+                // b = b * (x - X[j]) / (X[i] - X[j]);
+                int256[2] memory d_X_j = [X[j], X[j + 1]];
+                int256[2] memory d_X_i = [X[i], X[i + 1]];
+                b = DoubleMul(
+                    b,
+                    DoubleDiv(DoubleSub(x, d_X_j), DoubleSub(d_X_i, d_X_j))
+                );
+            }
+            y = DoubleAdd(y, b);
+        }
+
+    }
+
+    function setPrecise(int p) public {
+        dot = p;
+    }
+
+    // precision
+    int private dot = 100;
+
+    function Convert(int256[2] memory n)
+        public
+        view
+        returns (int256 r)
+    {
+        require(!(n[0] < 0 && n[1] < 0));
+        r = n[0] * dot + (n[0] < 0 ? -n[1] : n[1]);
+    }
+
+    function Normalize(int256 n)
+        public
+        view
+        returns (int256[2] memory r)
+    {
+        r = [n / dot, n > -dot ? n % dot : -n % dot];
+    }
+
+    function DoubleAdd(int256[2] memory x, int256[2] memory y)
+        public
+        view
+        returns (int256[2] memory z)
+    {
+        int256 sum = Convert(x) + Convert(y);
+        z = Normalize(sum);
+    }
+
+    function DoubleSub(int256[2] memory x, int256[2] memory y)
+        public
+        view
+        returns (int256[2] memory z)
+    {
+        int256 sum = Convert(x) - Convert(y);
+        z = Normalize(sum);
+    }
+
+    function DoubleDiv(int256[2] memory x, int256[2] memory y)
+        public
+        view
+        returns (int256[2] memory z)
+    {
+        int256 quot = Convert(x) * dot / Convert(y);
+        z = Normalize(quot);
+    }
+
+    function DoubleMul(int256[2] memory x, int256[2] memory y)
+        public
+        view
+        returns (int256[2] memory z)
+    {
+        int256 quot = Convert(x) * Convert(y) / dot;
+        z = Normalize(quot);
+    }
 }
--- test/polyTest.js
+++ test/polyTest.js
@@ -1,24 +1,162 @@
-const Poly = artifacts.require("Poly");
+const Poly = artifacts.require("Poly.sol");
 
 contract('Poly', (accounts) => {
 
-    before(async () => {
-        
+    const toNum = bn => bn.toNumber()
+
+    it(`InterpolateInt`, async () => {
+        const poly = await Poly.deployed();
+
+        const X = [0, 1, 4]
+        const Y = [-1, 1, 1]
+        const x = 6
+
+        let y = await poly.InterpolateInt(X, Y, x);
+        // console.log(y.toString());
     });
 
+    it(`Pconvert`, async () => {
+        const poly = await Poly.deployed();
+        const x = [-5, 6]
+
+        let res = await poly.Convert(x);
+        // console.log(res.toString());
+        assert.equal(toNum(res), -506, 'convert([-5, 6])');
+    });
+
+    it(`Add`, async () => {
+        const poly = await Poly.deployed();
+
+        const x = [5, 60]
+        const y = [5, 60]
+
+        let res = await poly.DoubleAdd(x, y);
+
+        assert.deepEqual(res.map(toNum), [11, 20], '5.6 + 5.6');
+    });
+
+    it(`Add`, async () => {
+        const poly = await Poly.deployed();
+
+        const x = [5, 06]
+        const y = [-5, 60]
+
+        let res = await poly.DoubleAdd(x, y);
+
+        assert.deepEqual(res.map(toNum), [0, -54], '5.06 + -5.60');
+    });
+
+    it(`Add`, async () => {
+        const poly = await Poly.deployed();
+
+        const x = [5, 06]
+        const y = [-6, 60]
+
+        let res = await poly.DoubleAdd(x, y);
+
+        assert.deepEqual(res.map(toNum), [-1, 54], '5.06 + -6.60');
+    });
+
+    it(`Sub`, async () => {
+        const poly = await Poly.deployed();
+
+        const x = [0, 56]
+        const y = [0, -60]
+
+        let res = await poly.DoubleSub(x, y);
+
+        assert.deepEqual(res.map(toNum), [1, 16], '0.56 - -0.60');
+    });
+
+    it(`Sub`, async () => {
+        const poly = await Poly.deployed();
+
+        const x = [0, -56]
+        const y = [0, 30]
+
+        let res = await poly.DoubleSub(x, y);
+
+        assert.deepEqual(res.map(toNum), [0, -86], '-0.56 - 0.30');
+    });
+
+    it(`Sub`, async () => {
+        const poly = await Poly.deployed();
+
+        const x = [1, 16]
+        const y = [2, 30]
+
+        let res = await poly.DoubleSub(x, y);
+
+        assert.deepEqual(res.map(toNum), [-1, 14], '1.16 - 2.30');
+    });
+
+    it(`Sub`, async () => {
+        const poly = await Poly.deployed();
+
+        const x = [1, 16]
+        const y = [1, 30]
+
+        let res = await poly.DoubleSub(x, y);
+
+        assert.deepEqual(res.map(toNum), [0, -14], '1.16 - 1.30');
+    });
+
+    it(`Div`, async () => {
+        const poly = await Poly.deployed();
+
+        const x = [1, 16]
+        const y = [1, 30]
+
+        let res = await poly.DoubleDiv(x, y);
+
+        assert.deepEqual(res.map(toNum), [0, 89], '1.16 / 1.30');
+    });
+
+    it(`Div`, async () => {
+        const poly = await Poly.deployed();
+
+        const x = [1, 16]
+        const y = [-1, 30]
+
+        let res = await poly.DoubleDiv(x, y);
+
+        assert.deepEqual(res.map(toNum), [0, -89], '1.16 / -1.30');
+    });
+
+    it(`Mul`, async () => {
+        const poly = await Poly.deployed();
+
+        const x = [1, 16]
+        const y = [-1, 30]
+
+        let res = await poly.DoubleMul(x, y);
+
+        assert.deepEqual(res.map(toNum), [-1, 50], '1.16 * -1.30');
+    });
+
+    it(`Mul`, async () => {
+        const poly = await Poly.deployed();
+
+        const x = [10000, 16]
+        const y = [0, 50]
+
+        let res = await poly.DoubleMul(x, y);
+
+        assert.deepEqual(res.map(toNum), [5000, 8], '10000.16 * 0.50');
+    });
 
     it(`Interpolate`, async () => {
+        const poly = await Poly.deployed();
 
-        const Poly = await Poly.deployed();
+        const X = [0, 0, 1, 0, 4, 0]
+        const Y = [-1, 0, 1, 0, 1, 0]
+        const x = [2, 50]
 
-        const X  = [0, 1, 4]
-        const Y  = [-1,1, 1]
-        const x = 5
-
-        let y = await Poly.Interpolate(X, Y, x);
-        
-        assert.equal(y, -1, 'ooops');
+        for (const p of [100, 1000, 10000, 100000]) {
+            await poly.setPrecise(p);
+            let d = await poly.Interpolate(X, Y, [2, parseInt(p/2)]);
+            console.log(p, d.map(toNum));
+        }
     });
-
 
 });
